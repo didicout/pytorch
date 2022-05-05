66dfda4d6dc1ffde795556db06a080a4cb239e81
Catherine Lee <csl@fb.com>
2022-05-05 13:34:51 -0700

    idk whats going on

--- .jenkins/pytorch/win-test-helpers/test_python_shard.bat
+++ .jenkins/pytorch/win-test-helpers/test_python_shard.bat
@@ -1,23 +1,23 @@
 call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
 :: exit the batch once there's an error
 if not errorlevel 0 (
-    echo "setup pytorch env failed"
-    echo %errorlevel%
-    exit /b
+  echo "setup pytorch env failed"
+  echo %errorlevel%
+  exit /b
 )
 
 pushd test
 if "%SHARD_NUMBER%" == "1" (
-    set GFLAGS_EXE="C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe"
-    if exist %GFLAGS_EXE% (
-        echo Some smoke tests
-        %GFLAGS_EXE% /i python.exe +sls
-        python %SCRIPT_HELPERS_DIR%\run_python_nn_smoketests.py
-        if ERRORLEVEL 1 goto fail
+  set GFLAGS_EXE="C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe"
+  if exist "%GFLAGS_EXE%" (
+    echo Some smoke tests
+    %GFLAGS_EXE% /i python.exe +sls
+    python %SCRIPT_HELPERS_DIR%\run_python_nn_smoketests.py
+    if ERRORLEVEL 1 goto fail
 
-        %GFLAGS_EXE% /i python.exe -sls
-        if ERRORLEVEL 1 goto fail
-    )
+    %GFLAGS_EXE% /i python.exe -sls
+    if ERRORLEVEL 1 goto fail
+  )
 )
 
 echo Copying over test times file
